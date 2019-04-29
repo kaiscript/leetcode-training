cbacd930eb2cd803eeac1ac95297352b2a952663
chenkai <chenkai1@yy.com>
2019-04-29 18:18:20 +0800

    20.21

---
+++ src/leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * 21. Merge Two Sorted Lists
+ * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ *
+ * Example:
+ *
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ *
+ * Created by chenkai on 2019/4/29.
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        temp.next = new ListNode(1);
+        System.out.println(head.next.val);
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+        mergeTwoSortedLists.mergeTwoLists(null, null);
+    }
+
+}
