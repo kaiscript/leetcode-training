0c24fa4169c0f03a004b3518ab95e29e8621c859
kaiscript <teardownk@gmail.com>
2019-05-22 22:14:17 +0800

    21

--- src/leetcode/MergeTwoSortedLists.java
+++ src/leetcode/MergeTwoSortedLists.java
@@ -24,9 +24,19 @@ public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode temp = head;
-        temp.next = new ListNode(1);
-        System.out.println(head.next.val);
-        return head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        temp.next = l1 == null ? l2 : l1;
+        return head.next;
     }
 
     public class ListNode {
