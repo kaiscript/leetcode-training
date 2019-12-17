019ccc33bf40c0e2876740cc2f1b7c28431d22e3
kaiscript <teardownk@gmail.com>
2019-12-18 00:18:35 +0800

    tree

---
+++ src/leetcode/tree/SymmetricTree.java
@@ -0,0 +1,97 @@
+package leetcode.tree;
+
+import leetcode.dfsbfs.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 101. Symmetric Tree
+ * Easy
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ *
+ * But the following [1,2,2,null,3,null,3] is not:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ *
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ */
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isS(root.left, root.right);
+    }
+
+    /**
+     * 分治递归
+     * @param left
+     * @param right
+     * @return
+     */
+    public boolean isS(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isS(left.left, right.right) && isS(right.left, left.right);
+    }
+
+    /**
+     * 迭代法。
+     * bfs的层次遍历
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetricIteratively(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            if (node1 == null && node2 == null) {
+                continue;
+            }
+            if (node1 == null || node2 == null) {
+                return false;
+            }
+            if (node1.val != node2.val) {
+                return false;
+            }
+            queue.add(node1.left);
+            queue.add(node2.right);
+            queue.add(node1.right);
+            queue.add(node2.left);
+        }
+        return true;
+    }
+
+}
