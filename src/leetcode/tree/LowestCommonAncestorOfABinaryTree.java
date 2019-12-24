37d7949823eb52b5b081933d8abe6367034ead9a
chenkai1 <chenkai1@yy.com>
2019-12-24 16:39:14 +0800

    LCA

---
+++ src/leetcode/tree/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,47 @@
+package leetcode.tree;
+
+/**
+ * 236. Lowest Common Ancestor of a Binary Tree
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ *
+ * Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ *
+ *
+ * Example 1:
+ *
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * Output: 3
+ * Explanation: The LCA of nodes 5 and 1 is 3.
+ * Example 2:
+ *
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * Output: 5
+ * Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author chenkai
+ * @date: 2019-12-24
+ */
+public class LowestCommonAncestorOfABinaryTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //递归出口
+        //从根节点递归地往下搜索，找到对应的节点则返回
+        if (root == null || p == root || q == root) {
+            return root;
+        }
+        //递归的拆解，往左右子树找到对应的节点，如果p和q分别在左右子树，则证明当前root节点为LCA
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        //如果左子树找不到对应节点则证明在右子树，返回右子树
+        return left == null ? right : left;
+    }
+
+}
