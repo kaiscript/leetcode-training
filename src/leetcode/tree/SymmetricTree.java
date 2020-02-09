5b321b7e513bc04b7b58db8d89132ee4d4d5d806
chenkai1 <chenkai1@yy.com>
2020-02-09 22:04:47 +0800

    tree

--- src/leetcode/tree/SymmetricTree.java
+++ src/leetcode/tree/SymmetricTree.java
@@ -7,28 +7,30 @@ import java.util.Queue;
  * 101. Symmetric Tree
  * Easy
  *
- * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * 给定一个二叉树，检查它是否是镜像对称的。
  *
- * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
  *
  *     1
  *    / \
  *   2   2
  *  / \ / \
  * 3  4 4  3
- *
- *
- * But the following [1,2,2,null,3,null,3] is not:
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
  *
  *     1
  *    / \
  *   2   2
  *    \   \
  *    3    3
+ * 说明:
  *
+ * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
  *
- * Note:
- * Bonus points if you could solve it both recursively and iteratively.
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  */
 public class SymmetricTree {
 
@@ -40,7 +42,8 @@ public class SymmetricTree {
     }
 
     /**
-     * 分治递归
+     * 分治递归.
+     * 判断是否堆成二叉树。就是判断左子树和右子树是否堆成。故递归的定义必须有2个参数，一个左节点，一个右节点
      * @param left
      * @param right
      * @return
