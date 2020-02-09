f8edc985789ce9d16c384db54d1e74f00e944053
chenkai1 <chenkai1@yy.com>
2020-02-09 23:06:35 +0800

    tree

--- src/leetcode/tree/ValidateBinarySearchTree.java
+++ src/leetcode/tree/ValidateBinarySearchTree.java
@@ -3,38 +3,36 @@ package leetcode.tree;
 /**
  * 98. Validate Binary Search Tree
  *
- * Given a binary tree, determine if it is a valid binary search tree (BST).
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
  *
- * Assume a BST is defined as follows:
+ * 假设一个二叉搜索树具有如下特征：
  *
- * The left subtree of a node contains only nodes with keys less than the node's key.
- * The right subtree of a node contains only nodes with keys greater than the node's key.
- * Both the left and right subtrees must also be binary search trees.
- *  
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
  *
- * Example 1:
- *
+ * 输入:
  *     2
  *    / \
  *   1   3
+ * 输出: true
+ * 示例 2:
  *
- * Input: [2,1,3]
- * Output: true
- * Example 2:
- *
+ * 输入:
  *     5
  *    / \
  *   1   4
  *      / \
  *     3   6
- *
- * Input: [5,1,4,null,null,3,6]
- * Output: false
- * Explanation: The root node's value is 5 but its right child's value is 4.
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ *      根节点的值为 5 ，但是其右子节点值为 4 。
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  */
 public class ValidateBinarySearchTree {
 
